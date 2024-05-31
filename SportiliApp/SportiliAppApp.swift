613148c52a9fbf1dfc46086df1532d460e10f1db
matthew-2000 <m.ercolino1@studenti.unisa.it>
2024-05-31 11:14:54 +0200

    Initial Commit

---
+++ SportiliApp/SportiliAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportiliAppApp.swift
+//  SportiliApp
+//
+//  Created by Matteo Ercolino on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SportiliAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
