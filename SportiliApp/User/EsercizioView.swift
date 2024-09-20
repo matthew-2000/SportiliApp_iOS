d33493b503dae16e345e93af3f04401ab90b8871
matthew-2000 <m.ercolino1@studenti.unisa.it>
2024-09-20 12:21:45 +0200

    add timer view

--- SportiliApp/User/EsercizioView.swift
+++ SportiliApp/User/EsercizioView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import FirebaseDatabase
+import AVFoundation
+import UIKit
 
 struct EsercizioView: View {
     
@@ -17,6 +19,8 @@ struct EsercizioView: View {
     @State private var showingAlert = false
     @State private var nota: String
     @StateObject var imageLoader = ImageLoader()
+
+    @State private var showTimerSheet = false
     
     init(giornoId: String, gruppoId: String, esercizioId: String, esercizio: Esercizio, showingAlert: Bool = false) {
         self.giornoId = giornoId
@@ -28,119 +32,111 @@ struct EsercizioView: View {
     }
     
     var body: some View {
-        VStack(alignment: .leading) {
-            
-            ZStack {
-                
-//                RoundedRectangle(cornerRadius: 10)
-//                    .foregroundColor(.cardGray)
-                
-                VStack(alignment: .leading) {
-                    
-                    if let image = imageLoader.image {
-                        Image(uiImage: image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(height: 250)
-                            .cornerRadius(5)
-                    } else {
-                        if imageLoader.error != nil {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 5)
-                                    .frame(height: 250)
-                                    .foregroundColor(.no)
-                                
-                                Text("Immagine non disponibile")
-                                    .montserrat(size: 20)
-                                
-                            }
+        ScrollView(.vertical, showsIndicators: false)  {
+            VStack(alignment: .leading) {
+                ZStack {
+                    VStack(alignment: .leading) {
+                        if let image = imageLoader.image {
+                            Image(uiImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(height: 250)
+                                .cornerRadius(5)
                         } else {
                             ZStack {
                                 RoundedRectangle(cornerRadius: 5)
                                     .frame(height: 250)
-                                    .foregroundColor(.no)
-                                
-                                // Visualizza uno spinner o un messaggio di caricamento
-                                ProgressView()
-                                    .progressViewStyle(CircularProgressViewStyle(tint: .accent))
-                                
+                                    .foregroundColor(.gray.opacity(0.2))
+                                if imageLoader.error != nil {
+                                    Text("Immagine non disponibile")
+                                        .montserrat(size: 20)
+                                } else {
+                                    ProgressView()
+                                        .progressViewStyle(CircularProgressViewStyle(tint: .accent))
+                                }
                             }
                         }
-                    }
-                    
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("\(esercizio.serie)")
-                            .montserrat(size: 30)
-                            .fontWeight(.bold)
-                            .foregroundColor(.accentColor)
-                        if let riposo = esercizio.riposo {
-                            if !riposo.isEmpty {
-                                Text("\(riposo) recupero")
-                                    .montserrat(size: 20)
-                            }
-                        }
-                        VStack(alignment: .leading, content: {
-                            Text("Note PT:")
-                                .montserrat(size: 15)
+                        
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("\(esercizio.serie)")
+                                .montserrat(size: 30)
                                 .fontWeight(.bold)
-                            if let notePT = esercizio.notePT {
-                                if !notePT.isEmpty {
+                                .foregroundColor(.accentColor)
+                            if let riposo = esercizio.riposo {
+                                if !riposo.isEmpty {
+                                    Text("\(riposo) recupero")
+                                        .montserrat(size: 20)
+                                }
+                            }
+                            
+                            VStack(alignment: .leading, content: {
+                                Text("Note PT:")
+                                    .montserrat(size: 15)
+                                    .fontWeight(.bold)
+                                if let notePT = esercizio.notePT, !notePT.isEmpty {
                                     Text(notePT)
                                         .montserrat(size: 15)
                                 } else {
                                     Text("Nessuna nota.")
                                         .montserrat(size: 15)
                                 }
-                            } else {
-                                Text("Nessuna nota.")
-                                    .montserrat(size: 15)
-                            }
+                            })
+                        }
+                        
+                        Spacer()
+                    }
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Note utente:")
+                        .montserrat(size: 15)
+                        .fontWeight(.bold)
+                    if let noteUtente = esercizio.noteUtente {
+                        Text(noteUtente)
+                            .montserrat(size: 15)
+                    } else {
+                        Text("Nessuna nota.")
+                            .montserrat(size: 15)
+                    }
+                    Spacer()
+                    Button(action: {
+                        showingAlert.toggle()
+                    }, label: {
+                        Text("Aggiungi Nota")
+                            .frame(maxWidth: .infinity)
+                    })
+                    .montserrat(size: 18)
+                    .buttonStyle(BorderedProminentButtonStyle())
+                    .controlSize(.large)
+                    .alert("Inserisci nota:", isPresented: $showingAlert) {
+                        TextField("Inserisci nota", text: $nota)
+                            .montserrat(size: 15)
+                        Button(action: addNota, label: {
+                            Text("Inserisci")
                         })
+                        .montserrat(size: 15)
+                    } message: {
+                        Text("Inserisci una nota per questo esercizio")
+                            .montserrat(size: 15)
                     }
                     
                     Spacer()
-
-                }
-//                .padding()
-            }
-            
-            VStack(alignment: .leading) {
-                Text("Note utente:")
-                    .montserrat(size: 15)
-                    .fontWeight(.bold)
-                if let noteUtente = esercizio.noteUtente {
-                    Text(noteUtente)
-                        .montserrat(size: 15)
-                } else {
-                    Text("Nessuna nota.")
-                        .montserrat(size: 15)
-                }
-                Spacer()
-                Button(action: {
-                    showingAlert.toggle()
-                }, label: {
-                    Text("Aggiungi Nota")
-                        .frame(maxWidth: .infinity)
-                })
-                .montserrat(size: 18)
-                .buttonStyle(BorderedProminentButtonStyle())
-                .controlSize(.large)
-                .alert("Inserisci nota:", isPresented: $showingAlert) {
-                    TextField("Inserisci nota", text: $nota)
-                        .montserrat(size: 15)
-                    Button(action: addNota, label: {
-                        Text("Inserisci")
+                    
+                    // Pulsante per aprire la Sheet con il Timer
+                    Button(action: {
+                        showTimerSheet.toggle()
+                    }, label: {
+                        Text("Avvia Timer di Recupero")
+                            .frame(maxWidth: .infinity)
                     })
-                    .montserrat(size: 15)
-                        
-                } message: {
-                    Text("Inserisci una nota per questo esercizio")
-                        .montserrat(size: 15)
+                    .montserrat(size: 18)
+                    .buttonStyle(BorderedProminentButtonStyle())
+                    .controlSize(.large)
                 }
+                
+                Spacer()
             }
-            
-            Spacer()
-            
+
         }
         .onAppear {
             let storagePath = "https://firebasestorage.googleapis.com/v0/b/sportiliapp.appspot.com/o/\(esercizio.name).png"
@@ -149,17 +145,17 @@ struct EsercizioView: View {
         .padding()
         .navigationTitle(esercizio.name)
         .navigationBarTitleDisplayMode(.large)
+        .sheet(isPresented: $showTimerSheet) {
+            TimerSheet(riposo: esercizio.riposo ?? "")
+        }
     }
     
     func addNota() {
-        
-        // Recupera il codice utente dal dispositivo
         guard let code = UserDefaults.standard.string(forKey: "code") else {
             print("Codice utente non trovato.")
             return
         }
         
-        // Riferimento al percorso Firebase
         let ref = Database.database().reference()
             .child("users")
             .child(code)
@@ -172,16 +168,151 @@ struct EsercizioView: View {
             .child(esercizioId)
             .child("noteUtente")
         
-        // Aggiornamento della nota utente
         ref.setValue(nota) { error, _ in
             if let error = error {
                 print("Errore nel salvataggio della nota utente: \(error.localizedDescription)")
-                // Puoi anche aggiungere un alert o altro feedback visivo qui per informare l'utente
             } else {
                 print("Nota utente salvata con successo")
-                // Puoi mostrare un messaggio di successo all'utente qui
             }
         }
     }
-    
+}
+
+struct TimerSheet: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var timeRemaining: Int
+    @State private var totalTime: Int
+    @State private var timerIsActive = false
+    @State private var timerPaused = false
+    @State private var timer: Timer?
+
+    // Aggiungi una proprietà per gestire l'audio
+    @State private var audioPlayer: AVAudioPlayer?
+
+    init(riposo: String) {
+        let parsedTime = TimerSheet.parseRiposo(riposo)
+        _timeRemaining = State(initialValue: parsedTime)
+        _totalTime = State(initialValue: parsedTime)
+    }
+
+    var body: some View {
+        NavigationView { // Aggiungi NavigationView qui
+            VStack {
+                Spacer()
+
+                Text("Tempo di recupero")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.bottom, 40)
+
+                // Animazione circolare che mostra il progresso
+                ZStack {
+                    Circle()
+                        .trim(from: 0, to: CGFloat(Double(timeRemaining) / Double(totalTime)))
+                        .stroke(Color.accentColor, lineWidth: 10)
+                        .frame(width: 200, height: 200)
+                        .rotationEffect(.degrees(-90)) // Rotazione per iniziare l'animazione dall'alto
+
+                    Text(formatTime(timeRemaining))
+                        .font(.system(size: 80, weight: .bold, design: .monospaced))
+                }
+                .padding()
+
+                Spacer()
+
+                // Pulsanti per controllare il timer
+                HStack {
+                    if timerIsActive {
+                        // Pulsante "Stop"
+                        Button(action: stopTimer) {
+                            Text("Stop")
+                                .frame(maxWidth: .infinity)
+                        }
+                        .montserrat(size: 18)
+                        .buttonStyle(BorderedProminentButtonStyle())
+                        .controlSize(.large)
+                    } else if timerPaused {
+                        // Pulsante "Riprendi"
+                        Button(action: startTimer) {
+                            Text("Riprendi")
+                                .frame(maxWidth: .infinity)
+                        }
+                        .montserrat(size: 18)
+                        .buttonStyle(BorderedProminentButtonStyle())
+                        .controlSize(.large)
+                    } else {
+                        // Pulsante "Inizia"
+                        Button(action: startTimer) {
+                            Text("Inizia")
+                                .frame(maxWidth: .infinity)
+                        }
+                        .montserrat(size: 18)
+                        .buttonStyle(BorderedProminentButtonStyle())
+                        .controlSize(.large)
+                    }
+                }
+
+                Spacer()
+            }
+            .padding()
+            .navigationBarItems(trailing: Button("Chiudi") {
+                presentationMode.wrappedValue.dismiss()
+            })
+            .interactiveDismissDisabled(true) // Disables swipe to dismiss
+        }
+    }
+
+    // Funzione per avviare il timer
+    func startTimer() {
+        if !timerIsActive {
+            timerIsActive = true
+            timerPaused = false
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                if timeRemaining > 0 {
+                    timeRemaining -= 1
+                } else {
+                    timer?.invalidate()
+                    timerIsActive = false
+                    playSound()
+                    triggerVibration()
+                }
+            }
+        }
+    }
+
+    // Funzione per stoppare il timer
+    func stopTimer() {
+        timer?.invalidate()
+        timerIsActive = false
+        timerPaused = true
+    }
+
+    // Funzione per convertire secondi in formato mm:ss
+    func formatTime(_ time: Int) -> String {
+        let minutes = time / 60
+        let seconds = time % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+
+    // Funzione per parsare il riposo (esempio formato "m'ss''")
+    static func parseRiposo(_ riposo: String) -> Int {
+        let components = riposo.split(separator: "'")
+        if components.count == 2 {
+            let minutes = Int(components[0]) ?? 0
+            let seconds = Int(components[1].replacingOccurrences(of: "\"", with: "")) ?? 0
+            return minutes * 60 + seconds
+        }
+        return 0
+    }
+
+    // Riproduci il suono quando il timer finisce
+    func playSound() {
+        AudioServicesPlaySystemSound(SystemSoundID(1022))
+    }
+
+    // Attiva la vibrazione quando il timer finisce
+    func triggerVibration() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
 }
